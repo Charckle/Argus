46a109f183212e0b4e60788747e67de598309286
charckle <andrej.zubin@email.com>
2019-12-07 21:26:18 +0100

    added indexing for wiki pages

--- argus.py
+++ argus.py
@@ -4,19 +4,25 @@ from whoosh.fields import Schema, TEXT
 from whoosh.index import open_dir
 from whoosh.query import *
 from whoosh.qparser import QueryParser
+from modules import web_module
 
 class WSearch():
     def __init__(self, storage_location = "data\\"):
         
         self.storage_location = storage_location
         
+    def create_index_object_file(self, schema):
+        if not os.path.exists(".index"):
+            os.mkdir(".index")
+        ix = create_in(".index", schema)
+        
+        return ix
+    
     def index_create(self):
         
         schema = Schema(file_name=TEXT(stored=True), content=TEXT)
         
-        if not os.path.exists(".index"):
-            os.mkdir(".index")
-        ix = create_in(".index", schema)
+        ix = self.create_index_object_file(schema)
         
         files = []
         # r=root, d=directories, f = files
@@ -36,6 +42,23 @@ class WSearch():
             #print(f)
             
         writer.commit()
+    
+    def index_create_from_wiki(self):
+        schema = Schema(file_name=TEXT(stored=True), content=TEXT)
+        
+        ix = self.create_index_object_file(schema)
+        
+        #get text data from web articles
+        articles = web_module.ih_web_scrap()
+        #
+        
+        writer = ix.writer()
+        
+        for link, data in articles:
+            
+            writer.add_document(file_name=u"{}".format(link), content=u"{}".format(data))
+        
+        writer.commit()
 
     def index_search(self,querystring):
         
@@ -52,5 +75,4 @@ class WSearch():
                 file_names.append(found["file_name"])
             
             return file_names
-        
-        
+
